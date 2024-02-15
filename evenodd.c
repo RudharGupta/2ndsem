050ea14cc317ddd5dd997c23ea6fc4ecf6a3a398
RudharGupta <147164167+RudharGupta@users.noreply.github.com>
2024-02-15 19:52:11 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -1,20 +1,8 @@
-/** 
- * C program to check even or odd using functions
- */
-
 #include <stdio.h>
-
-
-/**
- * Function to check even or odd
- * Returns 1 is num is even otherwise 0
- */
 int isEven(int num)
 {
     return !(num & 1);
 }
-
-
 int main()
 {
     int num;
